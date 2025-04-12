55eca5616164f4851bd39d72b487291272cd7df2
ericsonnascimento <ericson@grupocyber.com.br>
2025-04-12 11:23:23 -0300

    feature - Primeiro commit do projeto com criação da modules e criação da primeira classe personagem

---
+++ src/index.js
@@ -0,0 +1,18 @@
+import { Personagem } from "./modules/personagem.js"
+
+const personagemPedrinho = {
+    nome: 'Pedrinho',
+    vida: 7,
+    mana: 12,
+    level: 5,
+    tipo: 'Mago',
+}
+
+const personagemJose = {
+    nome: 'Jose',
+    vida: 7,
+    mana: 6,
+    level: 3,
+    tipo: 'Arqueiro',
+}
+
